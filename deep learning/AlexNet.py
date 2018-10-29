e7b8fc0d53e351eade0482c21f6e1d5259372ac6
YMaoJian <2854816010y@gmail.com>
2018-10-29 22:03:53 +0800

    Create AlexNet.py

---
+++ deep learning/AlexNet.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+class AlexNet(nn.Module):
+    def __init__(self, num_class = 1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Swquential(
+            nn.conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3,stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256, kernel_size=3, padding=1),
+            nn.ReLU(inpalce=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        def forward(self, x):
+            x = self.features(x)
+            x = x.view(x.size(0), 256 * 6 * 6)
+            x = self.classifier(x)
+            return x
